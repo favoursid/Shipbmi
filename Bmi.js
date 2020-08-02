c5ac118905a90249c45bdf0843518999d000c34f
favoursid <67465443+favoursid@users.noreply.github.com>
2020-08-02 12:46:51 +0100

    Created bmi html, bmi js, shipping html, shipping js and css form and css file

---
+++ Bmi.js
@@ -0,0 +1,30 @@
+function myInfo() {
+
+    // Collect weight from html
+    var weight = document.getElementById("weight").value;
+
+    // Collect height from html
+    var height = document.getElementById("height").value;
+
+    // Calculate bmi
+    var bmi = weight / (height * height)
+
+
+    // between 18.5 - 25 (optimal)
+    // < than 18.5 (underweght)
+    // > than 25 (overweght)
+
+    if (bmi >= 18.5 && bmi <= 25) {
+        var report = "Your Body Mass Index is: " + bmi + " this is between 18.5 and 25, so you are optimal";
+        document.getElementById("report").innerHTML
+
+    } else if (bmi < 18.5) {
+        var report = "Your Body Mass Index is: " + bmi + "this is less than 18.5, so you are underweight";
+        document.getElementById("report").innerHTML
+
+    } else {
+        var report = "Your Body Mass Index is: " + bmi + " this is greater than 25, so you are overweight";
+        document.getElementById("report").innerHTML
+
+        
+    }
---
+++ shipping.js
@@ -0,0 +1,31 @@
+function myParcel() {
+
+
+
+    // Collect Weight from html
+    var Weight = document.getElementById("Weightid").value;
+
+    // Collect Shipping Distance from html
+    var Distance = document.getElementById("Distanceid").value;
+
+    if (Weight <= 2) {
+        var cost = 1.10 * (Distance / 500)
+        document.getElementById("cost").innerHTML = cost;
+
+    } else if (Weight > 2 && weight <= 6) {
+        var cost = 2.20 * distance/500 
+        document.getElementById("cost").innerHTML = cost;
+
+    } else if (Weight > 6 && Weight <= 10) {
+        var cost = 3.70 * distance/500 
+        document.getElementById("cost").innerHTML = cost
+    } else {
+        var cost = "Shipping charge per 500 miles will be $3.80";
+        document.getElementById("cost")
+    } 
+
+
+}
+
+
+
